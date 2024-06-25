34004246c3dc35385297043d754752e96e7881ca
Coriolan <aldo.paniagua@hotmail.com>
2024-06-25 12:19:19 -0400

    Initial Commit

---
+++ Mobile Weather App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mobile Weather App
+//
+//  Created by Coriolan on 2024-06-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mobile Weather App/Mobile_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_Weather_AppApp.swift
+//  Mobile Weather App
+//
+//  Created by Coriolan on 2024-06-25.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
